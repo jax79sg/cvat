685fd9a6bdec3359078ac06476e99c7b372c8824
jax79sg <jax79sg@yahoo.com.sg>
2019-08-07 14:13:43 +0800

    [Saveannot] Added image save support

--- cvat/apps/saveannot/annotationgenerator.py
+++ cvat/apps/saveannot/annotationgenerator.py
@@ -13,34 +13,57 @@ from xml.dom import minidom
 import os
 import subprocess
 from . import dateutils
+basepath='/home/django/data/'
 
 def generateannotations(taskList=None, annotFormat='voc'):
    print("[AnnotaionGen] Tasklist: {}, Format:{}".format(taskList, annotFormat))
    for task in taskList:
       #Getting annotation xml and details
       taskannotationxmlobj,xmlstring=webapis.getTaskAnnotationXML(task)
-      sourcevideo = taskannotationxmlobj.getElementsByTagName('source')[0].firstChild.nodeValue
-      taskupdateddate = taskannotationxmlobj.getElementsByTagName('updated')[0].firstChild.nodeValue
-      deleteme=taskupdateddate
-      taskupdateddate=dateutils.converttolocalzone(taskupdateddate)
-      print("[AnnotaionGen] Soucevideo: {}".format(sourcevideo))
-      foldername=sourcevideo.split("/")[-1].split(".")[0]
-      print("[AnnotaionGen] foldername: {}".format(foldername))
-      folderpath=sourcevideo.split(foldername)[0]
+      mode = taskannotationxmlobj.getElementsByTagName('mode')[0].firstChild.nodeValue.lower()   
+      print("[AnnotaionGen] Mode: {}".format(mode))
+      
+      folderpath=basepath+task+'/.upload/'
       print("[AnnotaionGen] folderpath: {}".format(folderpath))
-
-	  #Creating the xml file
+      #Creating the xml file
       xmlfile=folderpath+task+".xml"
       f=open(xmlfile, "w")
       f.write(xmlstring)
       f.close()
       
+      if mode=='interpolation':
+         sourcevideo = taskannotationxmlobj.getElementsByTagName('source')[0].firstChild.nodeValue
+         print("[AnnotaionGen] Soucevideo: {}".format(sourcevideo))
+         
+         framesFolder=folderpath+'JPEGImages'
+         if (os.path.isdir(framesFolder)) == False:
+            print("[AnnotaionGen] Folder does not exist, creating now")
+            subprocess.call(['mkdir', framesFolder])
+            subprocess.call(['ffmpeg', '-i', sourcevideo, '-vsync','0','-start_number','0', framesFolder+'/'+task+'_%08d.jpg'])
+      elif mode=='annotation':
+         framesFolder=folderpath+'JPEGImages'
+         if (os.path.isdir(framesFolder)) == False:
+            print("[AnnotaionGen] Folder does not exist, creating now")
+            subprocess.call(['mkdir', framesFolder])
+            imgfolder=folderpath+'*.jpg'
+            subprocess.call(['cp',imgfolder, framesFolder])
+            imgfolder=folderpath+'*.png'
+            subprocess.call(['cp',imgfolder, framesFolder])
+      taskupdateddate = taskannotationxmlobj.getElementsByTagName('updated')[0].firstChild.nodeValue
+      taskupdateddate=dateutils.converttolocalzone(taskupdateddate)
+      
+      
+      
+      
+      
+      #foldername=sourcevideo.split("/")[-1].split(".")[0]
+      #print("[AnnotaionGen] foldername: {}".format(foldername))
+      
+      
+
+	  
+      
       #Generating the frames from video
-      framesFolder=folderpath+'JPEGImages'
-      if (os.path.isdir(framesFolder)) == False:
-         print("[AnnotaionGen] Folder does not exist, creating now")
-         subprocess.call(['mkdir', framesFolder])
-         subprocess.call(['ffmpeg', '-i', sourcevideo, '-vsync','0','-start_number','0', framesFolder+'/'+task+'_%08d.jpg'])
       
       #Calling converter
       if (annotFormat=='1'):
@@ -50,8 +73,7 @@ def generateannotations(taskList=None, annotFormat='voc'):
          subprocess.call(['rm','-r', vocxml])
          subprocess.call(['python3', '/home/django/cvat/utils/voc/converter.py', '--cvat-xml',xmlfile,'--image-dir',framesFolder,'--output-dir',vocxml])                  
          subprocess.call(['zip', '-r','-0', folderpath+task+'_'+taskupdateddate+'_VOC.zip', vocxml,vocjpeg])
-         
-      print(deleteme)
+        
       
       
       
